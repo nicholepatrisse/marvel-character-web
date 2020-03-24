564050f16858efab02135decce15268e00a5eba0
nicholepatrisse <nichole.patrisse@gmail.com>
2020-03-23 18:19:04 -0700

    Fix bug

--- util/api_util.js
+++ util/api_util.js
@@ -1,7 +1,7 @@
 const $ = require("jQuery");
 const MD5 = require("crypto-js/md5");
-const privateKey: "680747c71381930a32b071c07a8a55d07a874bcb";
-const publicKey: "af11c90b361c0c84785f26a81c8b450f";
+const privateKey = "680747c71381930a32b071c07a8a55d07a874bcb";
+const publicKey = "af11c90b361c0c84785f26a81c8b450f";
 let timestamp = Date.now();
 let hash = MD5(timestamp + privateKey + publicKey);
 
